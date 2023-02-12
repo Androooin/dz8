4c6a82b4d5c5578b2e4ee5614531aeaf80ba3bd1
Androooin <Androooin@yandex.ru>
2023-02-12 20:10:10 +0300

    начало

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+
+
+
+Console.WriteLine("Напишите любое целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+if (num % 2 == 0)
+{
+    while (index <= num)
+    {
+        Console.Write(index + " ");
+        index = index + 2;
+    }
+}
+else
+{
+    while (index <= num - 1)
+    {
+
+        Console.Write(index + " ");
+        index = index + 2;
+    }
+}
+
